32b526ff1f93bf02958a8bbe891a0c798689da34
developersunesis <developersunesis@gmail.com>
2021-08-19 07:30:55 +0100

    Ready for first release

--- src/Resources/lang2js.skeleton.js
+++ src/Resources/lang2js.skeleton.js
@@ -1,5 +1,5 @@
 /*!
- * Lang2Js - https://github.com/developersunesis
+ * Lang2Js - https://github.com/developersunesis/lang2js
  * Version - 0.0.1
  * Licensed under the MIT license - https://opensource.org/licenses/MIT
  */
