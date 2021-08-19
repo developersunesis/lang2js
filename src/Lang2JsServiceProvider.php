eff2661d7ae5c85e4a4393514da336a00ba69177
developersunesis <developersunesis@gmail.com>
2021-08-19 07:11:44 +0100

    Ready for first release

--- src/Lang2JsServiceProvider.php
+++ src/Lang2JsServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Developersunesis\Lang2js;
 
+use Developersunesis\Lang2js\Console\Lang2jsCommand;
 use Illuminate\Support\ServiceProvider;
 
 class Lang2JsServiceProvider extends ServiceProvider
 {
-
     public function register()
     {
         //
@@ -17,6 +17,11 @@ class Lang2JsServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        //
+        // Register the command if we are using the application via the CLI
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Lang2jsCommand::class,
+            ]);
+        }
     }
 }
